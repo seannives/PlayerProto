198b97d769f2251d046a796172db845f8d8e75ac
Mike Lippert <mlippert255@gmail.com>
2013-06-01 11:42:51 -0400

    Initial radiogroup widget

---
+++ js/widget-radiogroup.js
@@ -0,0 +1,203 @@
+/* **************************************************************************
+ * $Workfile:: widget-radiogroup.js                                         $
+ * **********************************************************************//**
+ *
+ * @fileoverview Implementation of the RadioGroup widget.
+ *
+ * The RadioGroup widget draws a list of choices and allows the user to
+ * select one of the choices.
+ *
+ * Created on		May 29, 2013
+ * @author			Michael Jay Lippert
+ *
+ * Copyright (c) 2013 Pearson, All rights reserved.
+ *
+ * **************************************************************************/
+
+// Sample configuration objects for classes defined here
+(function()
+{
+	var Q1Choices =
+	[
+		{
+			content: "Because as the population increases, the absolute number of births increases even though the growth rate stays constant.",
+			response: "Growth rate stays constant.",
+			answerKey: "correct"
+		},
+		{
+			content: "Because the growth rate increases as the population rises.",
+			response: "Does the growth rate change with population size?",
+			answerKey: "wrong1"
+		},
+		{
+			content: "Because the total fertility rate increases with population.",
+			response: "Does the fertility rate change with population size?",
+			answerKey: "wrong2"
+	
+		},
+		{
+			content: "Because social behaviors change and people decide to have more children.",
+			response: "This might happen but is it something is necessarily occurs?",
+			answerKey: "wrong3"
+		}
+	];
+	
+	// RadioButton widget config
+	var rbConfig =
+		{
+			id: "RG1",
+			choices: Q1Choices
+		};
+	
+	// RadioButtonQuestion widget config
+	var rbqConfig =
+	{
+		id: "Q1",
+		choices: Q1Choices,
+		type: "randomized", //default, even if not specified
+	};
+});
+
+/**
+ * Answers are presented to users by certain widgets that allow the user to
+ * select one (or more of them).
+ *
+ * @typedef {Object} Answer
+ * @property {string}	content		-The content of the answer, which presents the
+ * 									 meaning of the answer.
+ * @property {string}	response	-The response is presented to the user when
+ * 									 they choose this answer.
+ * @property {string}	answerKey	-This is the unique ID that will be returned
+ * 									 to the scoring engine to identify that the
+ * 									 user has chosen this answer.
+ *
+ * @todo: the content currently must be text (a string) however, we are likely
+ * to want to make the content be any widget.
+ */
+
+
+/* **************************************************************************
+ * RadioGroup                                                           *//**
+ *
+ * The RadioGroup widget draws a list of choices and allows the user to
+ * select one of the choices.
+ *
+ * @constructor
+ * @implements {IWidget}
+ *
+ * @param {Object}		config			-The settings to configure this RadioGroup
+ * @param {string|undefined}
+ * 						config.id		-String to uniquely identify this RadioGroup.
+ * 										 if undefined a unique id will be assigned.
+ * @param {Array.<Answer>}
+ *						config.choices	-The list of choices (answers) to be presented by the RadioGroup.
+ *
+ ****************************************************************************/
+function RadioGroup(config, eventManager)
+{
+	var that = this;
+	
+	/**
+	 * A unique id for this instance of the radio group widget
+	 * @type {string}
+	 */
+	this.id = getIdFromConfigOrAuto(config, RadioGroup);
+
+	/**
+	 * The list of choices presented by the RadioGroup.
+	 * @type {Array.<Answer>}
+	 */
+	this.choices = config.choices;
+
+	/**
+	 * The event manager to use to publish (and subscribe to) events for this widget
+	 * @type {EventManager}
+	 */
+	this.eventManager = eventManager;
+
+	/**
+	 * The event id published when an item in this carousel is selected.
+	 * @const
+	 * @type {string}
+	 */
+	this.selectedEventId = this.id + '_itemSelected';
+	
+	/**
+	 * The event details for this.selectedEventId events
+	 * @typedef {Object} SelectedEventDetails
+	 * @property {string} selectKey	-The answerKey associated with the selected answer.
+	 */
+
+	/**
+	 * Information about the last drawn instance of this image (from the draw method)
+	 * @type {Object}
+	 */
+	this.lastdrawn =
+		{
+			container: null,
+			widgetGroup: null,
+		};
+} // end of RadioGroup constructor
+
+/**
+ * Prefix to use when generating ids for instances of RadioGroup.
+ * @const
+ * @type {string}
+ */
+RadioGroup.autoIdPrefix = "rg_auto_";
+
+/* **************************************************************************
+ * RadioGroup.draw                                                      *//**
+ *
+ * Draw this RadioGroup in the given container.
+ *
+ * @param {!d3.selection}
+ *					container	-The container html element to append the radio
+ *								 group element tree to.
+ *
+ ****************************************************************************/
+RadioGroup.prototype.draw = function(container)
+{
+	this.lastdrawn.container = container;
+
+	var that = this;
+	
+	// make a div to hold the radio group
+	var widgetGroup = container.append("div")
+		.attr("class", "widgetRadioGroup")
+		.attr("id", this.id);
+
+	// We will use a table to provide structure for the radio group
+	// and put each answer in its own row of the table.
+	var table = widgetGroup.append("table")
+		.attr("class", "questionTable");
+
+	// add a colgroup w/ a class so each column can be styled in widgets.css
+	table.append("colgroup").attr("class", "button");
+	table.append("colgroup").attr("class", "content");
+
+	// create the table body to contain the answer rows
+	var tbody = table.append("tbody");
+
+	// Create a group for each item then draw the item in that group
+	var ansRows = tbody.selectAll("tr").data(this.choices);
+	ansRows.enter().append("tr");
+
+	var buttonCell = ansRows.append("td");
+	buttonCell
+		.text("A) ");
+
+	buttonCell
+		.append("input")
+			.attr("type", "radio")
+			.attr("name", this.id)
+			.attr("value", function (d) {return d.answerKey;});
+
+	ansRows
+		.append("td")
+			.text(function (d) {return d.content;});
+	
+	this.lastdrawn.widgetGroup = widgetGroup;
+
+}; // end of RadioGroup.draw()
+
