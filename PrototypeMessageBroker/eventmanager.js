400bd33be1eb3308576857cf0ee96fbb8b1e4f34
Young Suk Ahn Park <yahnpark>
2013-07-15 15:43:24 -0400

    Implement simple MessageBroker and extend the EventManager to work with MB. Test with Kantorowski page

---
+++ PrototypeMessageBroker/eventmanager.js
@@ -0,0 +1,166 @@
+/* **************************************************************************
+ * $Workfile:: eventmanager.js                                              $
+ * **********************************************************************//**
+ *
+ * @fileoverview Implementation of an EventManager object.
+ *
+ * The EventManager implements the Observer pattern, aka (Publish/Subscribe)
+ * as described here: http://msdn.microsoft.com/en-us/magazine/hh201955.aspx
+ * A javascript implementation of this pattern is available at:
+ * https://github.com/mroderick/PubSubJS
+ *
+ * Created on		March 18, 2013
+ * @author			Michael Jay Lippert
+ * @author			Young Suk Ahn Park
+ *
+ * **************************************************************************/
+
+/* **************************************************************************
+ * Constants
+ ****************************************************************************/
+
+/* **************************************************************************
+ * Page variables
+ ****************************************************************************/
+
+/* **************************************************************************
+ * EventManager                                                         *//**
+ *
+ * @constructor
+ *
+ * The event manager keeps track of subscribers of a particular topic (event)
+ * so that when a publisher publishes that topic all of the subscribers can
+ * be notified.
+ *
+ * The event manager should be used for all events. That will allow
+ * one widget on a page to respond to an event published (fired) by another
+ * widget on the page. It also will allow for multiple response to a single
+ * event.
+ *
+ ****************************************************************************/
+function EventManager()
+{
+	// Private Fields (should not be referenced except by EventManager methods)
+	
+	/**
+	 * events_ associates eventIds with an array of publishers and an array of
+	 * subscribers to that event.
+	 * @type Object.<string, ManagedEventInfo>
+	 * @private
+	 */
+	this.events_ = {};
+}
+
+/* **************************************************************************
+ * EventManager.subscribe                                               *//**
+ *
+ * EventManager class method to subscribe to an event that an object may fire.
+ *
+ * @param {string} eventId		The identifier of the event that when fired
+ *								should invoke the given callback. aka topic.
+ * @param {Function} handler	The function that will be called when the
+ *								event is fired.  
+ *
+ * Notes:
+ * - We'll need to create some unique token if we want to allow unsubscribe.
+ * - If you subscribe to the same callback multiple times, when the event is
+ *   fired it will be called once for each subscription.
+ ****************************************************************************/
+EventManager.prototype.subscribe = function(eventId, handler)
+{
+	// If the eventId has never been subscribed to, add it
+	if (!(eventId in this.events_))
+	{
+		this.events_[eventId] =  { handlers: [] };
+	}
+	
+	var event = this.events_[eventId];
+	
+	// Add the handler to the list of handlers of the eventId
+	event.handlers.push(handler);
+};
+
+
+/* **************************************************************************
+ * EventManager.publishLocal                                           *//**
+ *
+ * EventManager class method to publish (fire) an event calling the
+ * notification function of all subscribers of that event.
+ * This method does not send message to the MessageBroker.
+ *
+ * @param {string} eventId		The identifier of the event being fired.
+ *								aka topic.
+ * @param {Object} eventDetails	The details of the event to be passed to each
+ *								subscriber's notification function. Its value
+ *								is specific to the particular event.
+ *
+ ****************************************************************************/
+EventManager.prototype.publishLocal = function(eventId, eventDetails)
+{
+	// If there are no subscribers, do nothing
+	if (eventId in this.events_)
+	{
+		var event = this.events_[eventId];
+		
+		// Call all the subscribed notification functions for this event
+		for (var i = 0; i < event.handlers.length; ++i)
+		{
+			var handler = event.handlers[i];
+			handler(eventDetails);
+		}
+	}
+}
+
+/* **************************************************************************
+ * EventManager.publish                                                 *//**
+ *
+ * Besides publishing the event to the local subscribers, it also sends 
+ * to the MessageBroker which is an message listener at parent window.
+ *
+ * @param {string} eventId		The identifier of the event being fired.
+ *								aka topic.
+ * @param {Object} eventDetails	The details of the event to be passed to each
+ *								subscriber's notification function. Its value
+ *								is specific to the particular event.
+ *
+ ****************************************************************************/
+EventManager.prototype.publish = function(eventId, eventDetails)
+{
+	// If there are no subscribers, do nothing
+	this.publishLocal(eventId, eventDetails);
+
+	// YSAP - Send to the parent window as well.
+	// For the message structure see messagebroker.js
+	window.parent.postMessage(
+		{
+			messageType: "bricevent",
+			message: {
+				topic: eventId,
+	            eventData: eventDetails
+			}
+        }, '*');
+};
+
+/* **************************************************************************
+ * EventManager.listenBroker                                             *//**
+ *
+ * Start listening to the window's message event. 
+ * Only required if the iframe contains bric that listens to events.
+ * The iframe are supposed to invoke this method when all the objects are 
+ * ready to handle events. 
+ *
+ *
+ ****************************************************************************/
+EventManager.prototype.listenBroker = function()
+{
+	var _this = this;
+	window.addEventListener('message', function(e){
+        var data = e.data;
+        var here = location.href;
+        if (data.messageType === 'bricevent'){
+console.log("["+location.href+"] EventManager: Handling bricevent:" + JSON.stringify(data));
+			// Publish in the local iframe 
+			_this.publishLocal(data.message.topic, data.message.eventData, true);
+        }
+    });
+}
--- PrototypeMessageBroker/messagebroker.js
+++ PrototypeMessageBroker/messagebroker.js
@@ -1,29 +1,46 @@
-/* Message Broker
- */
+/**
+ * @fileoverview Implementation of a MessageBroker
 
-/**$
+ * MessageBroker is the component that lives in the master document (html) and 
+ * serves as "middleware" that relays messages between iframes.
  *
- * 
+ * For the mean time the implementation of brokering is very rudimentary.
+ * It just relays every 'bricevent' messages to the rest of the iframes except
+ * where the event was originated.
+ *
+ * The message format is as follows (the inner structure 'data' is the actual payload').
+ * Notice that the message contains the topic.
+
  Event = {
  	source: <source>,
  	data: {
  		messageType: <bricevent, resize>
  		#case bricevent, this is what actually sent at EventManager scope
-	 	payload: {
+	 	message: {
 	 		sendTime: <time was sent in unix format>
-	 		topic: <the event manager's topic>
-			sourceObjectId: <object Id where the event was originated>
-			eventName: <changed, submitted, control, error, etc> 
+	 		topic: <the event manager's topic: objectId/event_name (changed, submitted, control, error, etc)>
 			eventData: <specific data, usually collection of key-value pairs> 
 	 	}
 
 	 	#case resize
-	 	payload: {
-			width:<w>,
-			height: <h>
-	 	}
+		width:<w>,
+		height:<h>
 	 }
  }
+
+ *
+ * Created on		March 18, 2013
+ * @author			Young Suk Ahn Park
+ */
+
+/**
+ * MessageBroker 
+ * @constructor
+ *
+ * The MessageBroker all bric iframes for the message relaying.
+ * To make it simpler to migrate to object literal style, the constructor's 
+ * logic was placed in the initialize() function.
+ * 
  */
 var MessageBroker = function(options) {
 
@@ -31,63 +48,104 @@ var MessageBroker = function(options) {
 	this.bricIframes = null;
 
 
-	// Perform the actual call to the initialization method.
+	// Calls the initialization method.
 	// (Remember that we are already in constructor scope)
 	this.initialize.apply(this, arguments);
 }
 
-	// Centralize the initialization login in one single function
+/**
+ * MessageBroker.initialize
+ *
+ * The initialization method.
+ *
+ * @param {Object} options		Options (currently no option is used) .
+ * 
+ */
 MessageBroker.prototype.initialize = function (options) {
 
  		this.convertObjectTagToIframeTag();
 
-		//var widgetsOnPage = document.querySelectorAll('iframe');
-		this.bricIframes = $("iframe.bric");
+		this.bricIframes = document.querySelectorAll('iframe.bric'); // 
+		//this.bricIframes = $("iframe.bric");  // Using jQuery 
 
-		// Handle events.
-
+		// Listen to messages.
 		var _self = this;
         window.addEventListener('message', function(evt){
-console.log("Message Received")
-            if (evt.data.method === 'save') _self.relay(evt);
-            if (evt.data.method === 'resize') _self.resize(evt);
+console.log("[MB] Message Received: " + evt.data.messageType);
+            if (evt.data.messageType === 'bricevent') _self.relay(evt);
+            if (evt.data.messageType === 'resize') _self.resize(evt);
         });
 	};
 
 
 	////////// The rest of methods //////////
 
-	// Relay the message to the rest of iframes
+/**
+ * MessageBroker.initialize
+ *
+ * Relay the message to the rest of iframes.
+ * @todo: Possible improvement. Smarter relaying, i.e. to specific iframes 
+ *        that are interested in that topic.
+ *        Be ware that "smarter" MessageBroker means more load/complexity to it.
+ *
+ * @param {Object} evt		The event object as sent by the postMessage().
+ * 
+ */
 MessageBroker.prototype.relay = function (evt) {
 		
-        [].forEach.call(this.bricIframes, function(bricIframes){
-            // Skip over the widget that sent the message.
-            if (evt.source === bricIframes.contentWindow) return;
+        [].forEach.call(this.bricIframes, function(bricIframe){
+            // Skip over the iframe that sent the message.
+            if (evt.source === bricIframe.contentWindow) return;
 
             var message = {
-                method: 'restore',
-                value: evt.data.value
+            	messageType: evt.data.messageType,
+            	message: evt.data.message
             }
-            bricIframes.contentWindow.postMessage(message, '*')
+            bricIframe.contentWindow.postMessage(message, '*');
         });
 	};
 
+/**
+ * MessageBroker.resize
+ *
+ * Handles the resize of the iframes.
+ * The message contains the width and height.
+ * @todo: Check that all user agents (browsers) that we intent to support
+ *        behaves correctly.
+ *
+ * @param {Object} evt		The event object as sent by the postMessage().
+ */
 MessageBroker.prototype.resize = function (evt) {
         var sourceObject = findIFrameWithWindow(evt.source);
         sourceObject.style.width = evt.data.width + 'px';
         sourceObject.style.height = evt.data.height + 'px';
     };
 
+    ////////// methods related to DOM
+    // Maybe is a good idea to refactor them to to a separate class
+    // Say, MasterDocumentManager, that handles DOM related events.
+    // And provides abstraction of specific DOM manipulation
+
+/**
+ * MessageBroker.findIFrameWithWindow
+ *
+ * Returns the matching iframe within the the list of bric iframe list. 
+ *
+ * @param {Object} evt		The event object as sent by the postMessage().
+ */
 MessageBroker.prototype.findIFrameWithWindow = function (win){
         for (var i = 0; i < this.bricIframes.length; i++){
             if (win === this.bricIframes[i].contentWindow) return this.bricIframes[i];
         }
     };
 
-    ////////// methods related to DOM
-    // Probably will be refactored to MasterDocumentManager
-
-    // Method returns a queryString from <param> tags inside and <object>.
+/**
+ * MessageBroker.findIFrameWithWindow
+ *
+ * Returns a queryString from <param> tags inside and <object>. 
+ *
+ * @param {Node} objectNode		The object node that will be changed to iframe, and contains the params.
+ */
 function buildQueryStringFromParams(objectNode){
         var params = objectNode.querySelectorAll('param');
         var queryString = [].reduce.call(params, function(acc, paramNode){
@@ -100,6 +158,13 @@ function buildQueryStringFromParams(objectNode){
         return queryString;
     };
 
+/**
+ * MessageBroker.convertObjectTagToIframeTag
+ *
+ * COnverts the object tag to iframe tag.(Yes, as the function name implies) 
+ *
+ * @param {Node} objectNode		The object node that will be changed to iframe, and contains the params.
+ */
 MessageBroker.prototype.convertObjectTagToIframeTag = function () {
 		// Turn the <object> tags into <iframe> tags to work around webkit bug https://bugs.webkit.org/show_bug.cgi?id=75395.
 	    // Also append parameters to iframe url so they're accessible to the iframe implementation.
