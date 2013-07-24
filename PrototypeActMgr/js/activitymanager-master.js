117d2e3ecaff6269623d12a25b22345c3be04105
seannives <seann.ives@gmail.com>
2013-07-24 10:00:27 -0400

    initialization with iframes wip

---
+++ PrototypeActMgr/js/activitymanager-master.js
@@ -0,0 +1,132 @@
+/*
+
+    Here is a quick prototype of a "Master" ActivityManager.  This guy gets loaded on the page in an
+    iframe (or maybe just on the master page after post-Habitat conversion?), goes to get all of the
+    Activities from PAF (or the server) and then sends those out.
+
+    Eventually it'll listen for events from the other iframes to send data off to PAF.
+
+*/
+
+var activityManager = {
+
+    collectContent : function(contentURL) {
+        // THIS IS FAKE.
+        // This is where you'd call to the PAF hub (or whatever content repo) using the contentURL
+        // and return the JSON content (perhaps the enriched sequenceNode).
+        // Instead we'll just fake up some json.
+
+        // grab the id out of the contentURL so we can populate a couple of fake examples
+        var regPattern = /\d*$/;
+        var myid = regPattern.exec(contentURL);
+
+        // a function to build a bunch of basically the same config over and over
+        var returnConfig = function(sid, myImg) {
+            var myConfig = {
+                "master": {
+                    "brix": [
+                        { // SVG Container
+                            "xtype": "svgContainer",
+                            "id": "svg" + sid,
+                            "nodeid": "svg" + sid,
+                            "height": 310,
+                            "maxHt": 350,
+                            "actualSize": {height: 310, width: 680} // dupe from img above
+                        },
+                        { // Captioned Image                            
+                            "xtype": "captionedImage",
+                            "id": "cimg" + sid,
+                            "captionPosition": "below",
+                            "image": {
+                                "targetid": "svg" + sid,
+                                "id": "img" + sid,
+                                "URI": myImg,
+                                "caption": "Nuclear Reactor Schematic Diagram",
+                                "actualSize": {height: 310, width: 680}
+                            }
+                        },
+                        { // action - append image to container
+                            "xtype": "action",
+                            "id": "appendImg" + sid,
+                            "type": "append",
+                            "what": "cimg" + sid,
+                            "to": "svg" + sid,
+                            "config": {topPercentOffset: 0, leftPercentOffset: 0, heightPercent: 1, widthPercent: 1}
+                        }
+                    ]
+                }
+            };
+            return myConfig;
+        };
+
+        var imageHash = {
+            '200' : '../img/reactor.jpg',
+            '201' : '../img/sunEarth.jpg',
+            '202' : '../img/ultraviolet.jpg',
+            '203' : '../img/strata.jpg',
+            '204' : '../img/insolationMap.jpg',
+            '205' : '../img/hottopic-deepsea1en.jpg',
+            '206' : '../img/FG12_5_3.jpg',
+            '207' : '../img/endocrineSystem.jpg',
+            '208' : '../img/defGraph.jpg',
+            '209' : '../img/ch4_4.jpg'
+        };
+
+        // This is the config that drives the interactives.  It's then buried in the enriched sequence node
+        // below in "targetActivity"
+
+        var interactiveConfig = returnConfig(myid[0], imageHash[myid[0]]);
+
+        // let's pretend we have an "enriched" SequenceNode (step 5 in Assignment Startup sequence
+        // diagram: https://hub.pearson.com/confluence/display/AF/Assignment+Startup)
+        var sequenceNodeRich = {
+          "@context" : "http://purl.org/pearson/paf/v1/ctx/core/SequenceNodeRich",
+          "@type" : "SequenceNode",
+          "@id" : 'http://paf.hub.pearson.com/resources/sequences/' + myid[0] + '/nodes/1',
+          "nodeIndex" : 1,
+          "startTime" : Date(),
+          "parentSequence" : {
+            "@id" : 'http://paf.hub.pearson.com/resources/sequences/' + myid[0],
+            "user" : "http://idm.api.pearson.com/resources/identities/29d32894735b731",
+            "learningContext" : {
+              "@type" : "CourseSection",
+              "@id" : "urn:udson:pearson.com/sms/prod:course/jsmith38271"
+            },
+            "overallActivity" : 'http://repo.paf.pearson.com/resources/activity/' + myid[0]
+          },
+          "targetBinding" : {
+            "@id" : "http://repo.paf.pearson.com/resources/activity/4924948879517615/bindings/2",
+            "boundActivity" : "http://repo.paf.pearson.com/resources/activity/59239547267",
+            "activityFormat" : "application/vnd.pearson.qti.v2.asi.xml",
+            "activityTitle" : "We'll put this in the targetActivity, thank you.",
+            "credit" : "ForCredit",
+            "scoreConstraints" : {
+              "normalMaximum" : 10
+            }
+          },
+          "targetActivity" : interactiveConfig,
+          "aggregateResult" : {
+            "templateVariables" : []
+          },
+          "player" : {
+            "frameFrontend" : "http://example.com/myplayer",
+            "widgetStartPage" : "http://hub.paf.pearson.com/widgets/83282736384572/index.html"
+          },
+          "resultCollection" : 'http://hub.paf.pearson.com/resources/sequences/' + myid[0] + '/nodes/1/results'
+        };
+
+        return sequenceNodeRich;
+    },
+
+
+    initMasterActivityManager : function(eventManager) {
+        // 'that = this' avoids having to reference whatever we named our object (aM.) within this factory
+        var that = this;
+
+        window.onload = function() {
+            console.log("js/activitymanager-master.js - window.onload");
+            eventManager.publish("brixInit", { value: 'yay' });
+        };
+    }
+
+};
---
+++ PrototypeActMgr/js/messagebroker-multiImage.js
@@ -0,0 +1,159 @@
+/**
+ * @fileoverview Implementation of a MessageBroker
+
+ * MessageBroker is the component that lives in the master document (html) and 
+ * serves as "middleware" that relays messages between iframes.
+ *
+ * For the mean time the implementation of brokering is very rudimentary.
+ * It just relays every 'bricevent' messages to the rest of the iframes except
+ * where the event was originated.
+ *
+ * The message format is as follows (the inner structure 'data' is the actual payload').
+ * Notice that the message contains the topic.
+
+ Event = {
+ 	source: <source>,
+ 	data: {
+ 		messageType: <bricevent, resize>
+ 		#case bricevent, this is what actually sent at EventManager scope
+	 	message: {
+	 		sendTime: <time was sent in unix format>
+	 		topic: <the event manager's topic: objectId/event_name (changed, submitted, control, error, etc)>
+			eventData: <specific data, usually collection of key-value pairs> 
+	 	}
+
+	 	#case resize
+		width:<w>,
+		height:<h>
+	 }
+ }
+
+ *
+ * Created on		March 18, 2013
+ * @author			Young Suk Ahn Park
+ */
+
+/**
+ * MessageBroker 
+ * @constructor
+ *
+ * The MessageBroker all bric iframes for the message relaying.
+ * To make it simpler to migrate to object literal style, the constructor's 
+ * logic was placed in the initialize() function.
+ * 
+ */
+var MessageBroker = function(options) {
+
+	// List of iframes that contains brics
+	this.bricIframes = null;
+
+
+	// Calls the initialization method.
+	// (Remember that we are already in constructor scope)
+	this.initialize.apply(this, arguments);
+}
+
+/**
+ * MessageBroker.initialize
+ *
+ * The initialization method.
+ *
+ * @param {Object} options		Options (currently no option is used) .
+ * 
+ */
+MessageBroker.prototype.initialize = function (options) {
+
+		this.bricIframes = document.querySelectorAll('iframe.bric'); // 
+		//this.bricIframes = $("iframe.bric");  // Using jQuery 
+
+		// Listen to messages.
+		var _self = this;
+		window.addEventListener('message', function(evt){
+console.log("[MB] Message Received: " + evt.data.messageType);
+			if (evt.data.messageType === 'bricevent') _self.relay(evt);
+			if (evt.data.messageType === 'resize') _self.resize(evt);
+		});
+	};
+
+
+	////////// The rest of methods //////////
+
+/**
+ * MessageBroker.initialize
+ *
+ * Relay the message to the rest of iframes.
+ * @todo: Possible improvement. Smarter relaying, i.e. to specific iframes 
+ *        that are interested in that topic.
+ *        Be ware that "smarter" MessageBroker means more load/complexity to it.
+ *
+ * @param {Object} evt		The event object as sent by the postMessage().
+ * 
+ */
+MessageBroker.prototype.relay = function (evt) {
+
+		[].forEach.call(this.bricIframes, function(bricIframe){
+			// Skip over the iframe that sent the message.
+			if (evt.source === bricIframe.contentWindow) return;
+
+			var message = {
+				messageType: evt.data.messageType,
+				message: evt.data.message
+			}
+			bricIframe.contentWindow.postMessage(message, '*');
+		});
+	};
+
+/**
+ * MessageBroker.resize
+ *
+ * Handles the resize of the iframes.
+ * The message contains the width and height.
+ * @todo: Check that all user agents (browsers) that we intent to support
+ *        behaves correctly.
+ *
+ * @param {Object} evt		The event object as sent by the postMessage().
+ */
+MessageBroker.prototype.resize = function (evt) {
+		var sourceObject = findIFrameWithWindow(evt.source);
+		sourceObject.style.width = evt.data.width + 'px';
+		sourceObject.style.height = evt.data.height + 'px';
+	};
+
+	////////// methods related to DOM
+	// Maybe is a good idea to refactor them to to a separate class
+	// Say, MasterDocumentManager, that handles DOM related events.
+	// And provides abstraction of specific DOM manipulation
+
+/**
+ * MessageBroker.findIFrameWithWindow
+ *
+ * Returns the matching iframe within the the list of bric iframe list. 
+ *
+ * @param {Object} evt		The event object as sent by the postMessage().
+ */
+MessageBroker.prototype.findIFrameWithWindow = function (win){
+		for (var i = 0; i < this.bricIframes.length; i++){
+			if (win === this.bricIframes[i].contentWindow) return this.bricIframes[i];
+		}
+	};
+
+/**
+ * MessageBroker.findIFrameWithWindow
+ *
+ * Returns a queryString from <param> tags inside and <object>. 
+ *
+ * @param {Node} objectNode		The object node that will be changed to iframe, and contains the params.
+ */
+function buildQueryStringFromParams(objectNode){
+		var params = objectNode.querySelectorAll('param');
+		var queryString = [].reduce.call(params, function(acc, paramNode){
+			var name = paramNode.getAttribute('name');
+			var value = paramNode.getAttribute('value');
+
+			if (acc) acc += '&';
+			return acc + encodeURIComponent(name) + '=' + encodeURIComponent(value);
+		}, '');
+		return queryString;
+	};
+
+
